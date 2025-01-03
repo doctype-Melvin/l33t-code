2833ed7ec74b8c25ae7cbc19584ec94e3de0049d
Melvin <71100019+doctype-Melvin@users.noreply.github.com>
2025-01-03 10:17:56 +0100

    Time: 722 ms (29.14%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -15,6 +15,8 @@ rank the rows by date
 
 */
 
+
+/*
 with ranked_records as (
     select
         id
@@ -30,4 +32,11 @@ left join ranked_records previous
     on current.row_rank = previous.row_rank + 1
     and DATEDIFF(current.recordDate, previous.recordDate) = 1
 where current.temperature > previous.temperature
+;
+*/
+
+select w2.id as 'Id'
+from weather w1
+join weather w2 on w1.recordDate = w2.recordDate - Interval 1 day
+where w1.temperature < w2.temperature
 ;
