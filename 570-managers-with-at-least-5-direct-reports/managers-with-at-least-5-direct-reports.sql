0c1c3b30e3161c65b2b78d191b0fd5bf36ccd757
Melvin <71100019+doctype-Melvin@users.noreply.github.com>
2025-04-10 12:22:54 +0200

    Time: 374 ms (77.29%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -2,16 +2,7 @@
 
 select e1.name
 from Employee e1
-left join Employee e2
+join Employee e2
 on e1.id = e2.managerId
-group by e1.id, e1.name
-having count(e2.id) >= 5
-;
-
-
-/*
-SELECT e1.name AS name
-FROM Employee e1
-JOIN Employee e2 ON e1.id = e2.managerId
-GROUP BY e1.id, e1.name
-HAVING COUNT(e2.id) >= 5;*/
+group by e1.id
+having count(e1.id) >= 5;
