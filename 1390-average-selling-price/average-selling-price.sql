ddbd6547ce271640477f702b13e8169efe8fc0d6
Melvin <71100019+doctype-Melvin@users.noreply.github.com>
2025-04-18 15:04:41 +0200

    Time: 934 ms (63.67%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -3,22 +3,10 @@
 select p.product_id, 
 round(
     ifnull(sum(p.price*s.units)/sum(s.units), 0)
-    , 2) as average_price
+    , 2) 
+    as average_price
 from Prices p
 left join UnitsSold s
 on p.product_id = s.product_id
 and s.purchase_date between p.start_date and p.end_date
 group by p.product_id;
-
-/*
-SELECT 
-    p.product_id,
-    ROUND(
-        IFNULL(SUM(u.units * p.price) / SUM(u.units), 0)
-    , 2) AS average_price
-FROM Prices p
-LEFT JOIN UnitsSold u
-  ON p.product_id = u.product_id
- AND u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY p.product_id;
-*/
