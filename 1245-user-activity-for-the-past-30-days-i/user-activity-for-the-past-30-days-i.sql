00149a909a53f202f83a3317c65c8384ffdae30b
Melvin <71100019+doctype-Melvin@users.noreply.github.com>
2025-04-21 12:15:39 +0200

    Time: 514 ms (72.24%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -3,5 +3,8 @@ select
 activity_date as day
 , count(distinct user_id) as active_users
 from Activity
+# using date add and a negative interval value to go back 30 days
+# both the lower threshold and the upper threshold is included in 'between'
+# you could also use 2019-07-27 with -29 day interval
 where activity_date between date_add("2019-07-28", interval -30 day) and "2019-07-27"
 group by day
