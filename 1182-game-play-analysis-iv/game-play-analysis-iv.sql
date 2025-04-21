7b0d7b0ca3914923ad801b558280aa3a863dc9b8
Melvin <71100019+doctype-Melvin@users.noreply.github.com>
2025-04-21 11:10:58 +0200

    Time: 714 ms (59.75%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# find first login date for all players
 with first_date as (
     select
     player_id
@@ -6,6 +7,7 @@ with first_date as (
     from Activity
     group by player_id
 ),
+# create table with players having a login the following day
 consecutive as (
     select 
     distinct a.player_id
@@ -15,6 +17,7 @@ consecutive as (
     where a.event_date = date_add(f.first_login, interval 1 day)
 )
 
+# join CTEs and calculate the fraction
 select
 round(count(c.player_id)/count(f.player_id), 2) as fraction
 from first_date f
