bef57132c0f3850ecf33886cb4b1c75c898bb868
Melvin <71100019+doctype-Melvin@users.noreply.github.com>
2025-04-27 07:37:56 +0200

    Time: 40 ms (69.19%) | Memory: 53.4 MB (59.78%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    
+        let calls = 0
+    return function(...args){
+        if (calls > 0) {
+            return
+        } 
+        else 
+        {
+           let result = fn(...args)
+            calls += 1
+            return result
+        }
+
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
