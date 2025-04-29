d96acc8d26d56d82f67a370e0f47523695f7683b
Melvin <71100019+doctype-Melvin@users.noreply.github.com>
2025-04-29 16:27:50 +0200

    Time: 913 ms (63.32%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
+# Bit of googling for this solution :( 
 with run_total as (
     select
     person_name
     ,person_id
+    # Window function over is quite nice
     , sum(weight) over (order by turn) as total_weight
     from Queue
 )
@@ -13,4 +15,5 @@ from Queue q
 join run_total r
 on q.person_id = r.person_id
 where r.total_weight <= 1000
+# cute way of coming up with the single row
 order by r.total_weight desc limit 1
