41d0647e59b855259b7d3ff709fd682d1f2c9012
Melvin <71100019+doctype-Melvin@users.noreply.github.com>
2025-05-16 09:28:16 +0200

    Time: 136 ms (46.32%) | Memory: 89.2 MB (54.19%) - LeetSync

--- 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
+++ 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
@@ -4,15 +4,16 @@
  * @return {Array}
  */
 var flat = function (arr, n) {
-    if (n === 0 || arr.every(item => !Array.isArray(item))) return arr
+    if (n === 0 || arr.forEach(item => !Array.isArray(item))) return arr
 
-    const result = []
-    for ( let i = 0; i < arr.length; i++ ) {
-        if (Array.isArray(arr[i]) && n > 0) {
-            result.push(...flat(arr[i], n-1))
-        } else {
+    let result = []
+    for (let i = 0 ; i < arr.length ; i++) {
+        if (!Array.isArray(arr[i])) {
             result.push(arr[i])
+        } else {
+            result.push(...flat(arr[i], n-1))
         }
     }
-        return result
+
+    return result
 };
